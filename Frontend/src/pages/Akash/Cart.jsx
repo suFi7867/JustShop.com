003a756864d578c4924b6d59492d0a77896772f5
Aakash928 <asassss337@gmail.com>
2022-11-10 21:09:49 +0530

    cart

--- Frontend/src/pages/Akash/Cart.jsx
+++ Frontend/src/pages/Akash/Cart.jsx
@@ -6,67 +6,71 @@ import {
   Link,
   Stack,
   useColorModeValue as mode,
-} from '@chakra-ui/react'
-import * as React from 'react'
-import { CartItem } from './CartItem'
-import { CartOrderSummary } from './CartOrderSummary'
-import { cartData } from './_data'
-
-const Cart = () => (
-  <Box
-    maxW={{
-      base: '3xl',
-      lg: '7xl',
-    }}
-    mx="auto"
-    px={{
-      base: '4',
-      md: '8',
-      lg: '12',
-    }}
-    py={{
-      base: '6',
-      md: '8',
-      lg: '12',
-    }}
-  >
-    <Stack
-      direction={{
-        base: 'column',
-        lg: 'row',
-      }}
-      align={{
-        lg: 'flex-start',
-      }}
-      spacing={{
-        base: '8',
-        md: '16',
-      }}
-    >
-      <Stack
-        spacing={{
-          base: '8',
-          md: '10',
-        }}
-        flex="2"
-      >
-        <Heading fontSize="2xl" fontWeight="extrabold">
-          Shopping Cart (3 items)
-        </Heading>
-
-        <Stack spacing="6">
-          {cartData.map((item) => (
-            <CartItem key={item.id} {...item} />
-          ))}
-        </Stack>
-      </Stack>
+} from "@chakra-ui/react";
+import * as React from "react";
+import { CartItem } from "./CartItem";
+import { CartOrderSummary } from "./CartOrderSummary";
+import { cartData } from "./_data";
 
 const Cart = () => {
   return (
-    <div>
-      <h1>Akash</h1>
-    </div>
-  )
-}
+    <Box
+      maxW={{
+        base: "3xl",
+        lg: "7xl",
+      }}
+      mx="auto"
+      px={{
+        base: "4",
+        md: "8",
+        lg: "12",
+      }}
+      py={{
+        base: "6",
+        md: "8",
+        lg: "12",
+      }}
+    >
+      <Stack
+        direction={{
+          base: "column",
+          lg: "row",
+        }}
+        align={{
+          lg: "flex-start",
+        }}
+        spacing={{
+          base: "8",
+          md: "16",
+        }}
+      >
+        <Stack
+          spacing={{
+            base: "8",
+            md: "10",
+          }}
+          flex="2"
+        >
+          <Heading fontSize="2xl" fontWeight="extrabold">
+            Shopping Cart (3 items)
+          </Heading>
 
-export default Cart
+          <Stack spacing="6">
+            {cartData.map((item) => (
+              <CartItem key={item.id} {...item} />
+            ))}
+          </Stack>
+        </Stack>
+        <Flex direction="column" align="center" flex="1">
+        <CartOrderSummary />
+        <HStack mt="6" fontWeight="semibold">
+          <p>or</p>
+          <Link color={mode('blue.500', 'blue.200')}>Continue shopping</Link>
+        </HStack>
+      </Flex>
+      </Stack>
+    </Box>
+  );
+};
+
+export default Cart;
